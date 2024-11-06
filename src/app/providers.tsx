af6fd21073af5fce81de32b11453252465aefb5f
Jovan Aleksic <jovan@defichain-ac.com>
2024-11-06 18:03:49 +0100

    switching to polygon

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,7 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { sepolia } from "wagmi/chains";
+import { polygon } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { AuthProvider } from "@/app/dashboard/auth/AuthContext";
 import { BlockchainProvider } from "@/context/BlockchainContext";
@@ -13,7 +13,7 @@ import { BlockchainProvider } from "@/context/BlockchainContext";
 const config = getDefaultConfig({
   appName: "Phoenix Shinobi",
   projectId: "9ddcb68482bc223eb1614b0a7d353e57",
-  chains: [sepolia],
+  chains: [polygon],
   ssr: true
 });
 
