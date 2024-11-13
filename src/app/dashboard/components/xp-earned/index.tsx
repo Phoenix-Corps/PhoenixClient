606e193dc40a2776ea5526bc507a9496b67f199a
milosj1992 <milosje1992@gmail.com>
2024-11-13 11:46:42 +0100

    xp earned fixed to loook like label

--- src/app/dashboard/components/xp-earned/index.tsx
+++ src/app/dashboard/components/xp-earned/index.tsx
@@ -71,8 +71,8 @@ const XPearned: React.FC = () => {
   return (
     <div className="profile-card-black bg-dark-card-grad pb-5 rounded-b-[40px] md:rounded-b-[60px] h-fit lg:rounded-b-[80px] shadow-xl px-[56px] font-noto-serif relative">
       <div className="flex flex-col gap-5 justify-center items-center pt-4">
-        <span className="yellow-button flex items-center gap-1 justify-center text-[#182C45] rounded-full text-sm p-2 lg:text-[18px] md:h-[45px] w-[189px] border-[4px] border-[#F2E63D] uppercase font-bold">
-          xp earned <VECTOR_1 alt="Vector Icon" />
+        <span className="yellow-button flex items-center gap-1 justify-center text-[#182C45] text-sm p-2 lg:text-[18px] md:h-[45px] w-[189px] border-[#F2E63D] uppercase font-bold border-0 rounded-sm">
+          xp earned 
         </span>
       </div>
 
@@ -91,14 +91,14 @@ const XPearned: React.FC = () => {
             </div>
           </div>
           <button
-            onClick={!isClickable ? upgradeLevel : undefined}
+            onClick={isClickable ? upgradeLevel : undefined}
             className={`
         flex items-center justify-center m-auto mt-4
         rounded-full text-sm p-2 lg:text-[18px] md:h-[45px] w-[189px]
         uppercase font-bold
         transition-colors duration-500 ease-in-out
         ${
-          !isClickable
+          isClickable
             ? "bg-gray-400 text-white border border-white"
             : " text-gray-700 border border-gray-400 cursor-not-allowed"
         }
