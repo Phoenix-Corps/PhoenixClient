d0717b29069c8b6edae333920b7059bad6b469a6
milosj1992 <milosje1992@gmail.com>
2024-11-16 22:32:13 +0100

    shinobi-link-added-top

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,11 +3,18 @@
 import Logo from "../../../public/Logo.svg";
 import Down from "../../../public/Down indicator.svg";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Hero = () => {
 
   return (
-    <section>
+    <section className="sm:text-right text-center">
+      <span className="button-container button-container-yellow sm:m-[2%]">
+          <Link className="button button-yellow" href="/dashboard">
+            <span className="button-label">Sign up for the</span>
+            <span className="button-text">Shinobi alliance</span>
+          </Link>
+        </span>
       <Logo className="w-[100%] h-full object-contain hero-image" />
       <Down style={{ margin: "auto" }} />
     </section>
