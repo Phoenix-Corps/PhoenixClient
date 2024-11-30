58aa8aad2fb3f29c22763d6188789d07fd4d5cd0
milosj1992 <milosje1992@gmail.com>
2024-11-30 03:42:06 +0100

    fix

--- src/app/buy/components/SellerLinkBar.tsx
+++ src/app/buy/components/SellerLinkBar.tsx
@@ -19,7 +19,7 @@ export const SellerLinkBar = (props: {
         <div className="aeroport text-[10px] md:w-auto w-full">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
         </div>
-        <button onClick={() => handleCopyCode(props.url)} >
+        <button onClick={() => handleCopyCode(props.url)}  className="md:w-auto w-full">
             <div className="relative p-2 md:w-[200px] text-[16px] flex flex-row gap-[5px] bg-[rgba(0,255,255,0.15)] w-full">
                 {props.url}
                 <Image
