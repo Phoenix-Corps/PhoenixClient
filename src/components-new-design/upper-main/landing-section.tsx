b9b334894f07b3f3b5302aee61ca54ab641f42e0
Jovan Aleksic <jovan@defichain-ac.com>
2024-12-01 13:26:31 +0100

    fix clicking shinobi button on mobile

--- src/components-new-design/upper-main/landing-section.tsx
+++ src/components-new-design/upper-main/landing-section.tsx
@@ -5,7 +5,7 @@ import DownArrow from "@public/home/down-arrow.png";
 
 export const LandingSection = () => {
     return <div className="h-screen">
-        <div className="w-[100%] h-[70%] md:h-[80%] md:w-[90%] relative m-auto ">
+        <div className="w-[100%] h-[70%] md:h-[80%] md:w-[90%] relative m-auto pointer-events-none">
             <div className="main-background-image absolute h-[100%] w-[100%] left-0 right-0 m-auto z-[1]" />
         </div>
         <div className="w-[80%] flex m-auto gap-[24px] md:mt-0 mt-4 flex-wrap justify-center">
